e59b167add588ab3df69f294a62413130324e2fe
Luc <luc@lucemans.nl>
2023-06-20 16:58:44 +0000

    Introduce Basic Web

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const metadata = {
+    title: 'V3X Company',
+    description: 'We do what we must because we can.',
+};
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
+}
