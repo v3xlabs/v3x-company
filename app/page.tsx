6638aa684603032a3d1aa8ffc3570723c3fcb42c
Luc <luc@lucemans.nl>
2023-06-24 07:48:51 +0000

    Introduce miniprojects

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
 import { FiGithub } from 'react-icons/fi';
 
+import { ProjectBox } from '../components/ProjectBox/ProjectBox';
+
 export default function App() {
     return (
         <div className="mx-auto flex items-center justify-center px-4 md:px-8">
-            <div
-                className="grid w-full"
-                style={{
-                    gridTemplateRows: 'masonry',
-                    gridTemplateColumns: 'repeat(12, 1fr)',
-                    gap: '1rem',
-                }}
-            >
+            <div className="grid w-full dagrid">
                 <div className="relative w-full bg-blue-400 col-span-7 row-span-3 rounded-md">
                     <div className="absolute inset-0 bg-gradient-to-tr from-black/5 to-white/20"></div>
                     <div className="absolute left-8 top-8 right-8 flex justify-between">
@@ -41,10 +36,90 @@ export default function App() {
                 </div>
                 <div className="w-full bg-pink-200 col-span-5 h-[512px] row-span-2 rounded-md" />
                 <div className="w-full bg-gray-200 h-48 col-span-5 row-span-1 rounded-md" />
-                <div className="w-full bg-gray-200 h-64 col-span-4 rounded-md" />
-                <div className="w-full bg-gray-200 h-64 col-span-4 rounded-md" />
-                <div className="w-full bg-gray-200 h-64 col-span-4 rounded-md" />
-                <div className="w-full bg-gray-200 h-64 col-span-12 rounded-md" />
+
+                <ProjectBox
+                    img="https://github.com/v3xlabs/.github/raw/master/projects/ens-tools-small.png#1"
+                    url="https://github.com/v3xlabs/ens-tools"
+                    title="ENS Tools"
+                    className="col-span-4"
+                />
+                <ProjectBox
+                    img="https://github.com/v3xlabs/.github/raw/master/projects/scyllo-small.png#1"
+                    url="https://github.com/v3xlabs/scyllo"
+                    title="Scyllo"
+                    className="col-span-4"
+                />
+                <ProjectBox
+                    img="https://github.com/v3xlabs/.github/raw/master/projects/edgeserver-small.png#1"
+                    url="https://github.com/v3xlabs/edgeserver"
+                    title="EdgeServer"
+                    className="col-span-4"
+                />
+                <ProjectBox
+                    img="https://github.com/v3xlabs/.github/raw/master/projects/signed-rsvp-small.png#1"
+                    url="https://github.com/v3xlabs/signed-rsvp"
+                    title="Signed RSVP"
+                />
+                <ProjectBox
+                    img="https://github.com/v3xlabs/.github/raw/master/projects/worldname-small.png#1"
+                    url="https://github.com/v3xlabs/worldcoin.name"
+                    title="Worldcoin.name"
+                />
+                <ProjectBox
+                    img="https://github.com/v3xlabs/.github/raw/master/projects/sunflake-small.png#1"
+                    url="https://github.com/v3xlabs/sunflake"
+                    title="Sunflake"
+                />
+                <ProjectBox
+                    img="https://github.com/v3xlabs/.github/raw/master/projects/logger-small.png#1"
+                    url="https://github.com/v3xlabs/logger"
+                    title="Logger"
+                />
+                <ProjectBox
+                    img="https://github.com/v3xlabs/.github/raw/master/projects/redeez-small.png#1"
+                    url="https://github.com/v3xlabs/redeez"
+                    title="Redeez"
+                />
+                <ProjectBox
+                    img="https://github.com/v3xlabs/.github/raw/master/projects/permissio-small.png#1"
+                    url="https://github.com/v3xlabs/permissio"
+                    title="Permissio"
+                />
+                <ProjectBox
+                    img="https://github.com/v3xlabs/.github/raw/master/projects/eslint-small.png#1"
+                    url="https://github.com/v3xlabs/eslint-v3xlabs"
+                    title="ESLint"
+                />
+                <ProjectBox
+                    img="https://github.com/v3xlabs/.github/raw/master/projects/node-fullykiosk-small.png#1"
+                    url="https://github.com/v3xlabs/node-fullykiosk"
+                    title="Node FullyKiosk"
+                />
+                <ProjectBox
+                    img="https://github.com/v3xlabs/.github/raw/master/projects/eth-classify-small.png#1"
+                    url="https://github.com/v3xlabs/eth-classify"
+                    title="ETH Classify"
+                />
+                <ProjectBox
+                    img="https://github.com/v3xlabs/.github/raw/master/projects/cache-fns-small.png#1"
+                    url="https://github.com/v3xlabs/cache-fns"
+                    title="Cache Fns"
+                />
+                <ProjectBox
+                    img="https://github.com/v3xlabs/.github/raw/master/projects/error-fns-small.png#1"
+                    url="https://github.com/v3xlabs/error-fns"
+                    title="Error Fns"
+                />
+                <ProjectBox
+                    img="https://github.com/v3xlabs/.github/raw/master/projects/key-computer-small.png#1"
+                    url="https://key.computer"
+                    title="Key Computer"
+                />
+                <ProjectBox
+                    img="https://github.com/v3xlabs/.github/raw/master/projects/ghchart-small.png#1"
+                    url="https://github.com/v3xlabs/ghchart"
+                    title="Github Chart"
+                />
             </div>
         </div>
     );
---
+++ components/ProjectBox/ProjectBox.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react';
+
+export const ProjectBox: FC<{
+    img: string;
+    url: string;
+    title: string;
+    className?: string;
+}> = ({ img, url, title, className }) => {
+    return (
+        <div
+            className={`hover:shadow-md rounded-lg overflow-hidden ${
+                className ?? 'col-span-3'
+            }`}
+        >
+            <a href={url} target="_blank" className="w-full h-full">
+                <img
+                    src={img}
+                    alt={title}
+                    className="w-full h-full object-cover"
+                />
+            </a>
+        </div>
+    );
+};
