26356076455b04ad92e0f7735ef3bd3f4bff0fad
Luc <luc@lucemans.nl>
2023-06-24 00:26:21 +0000

    Introduce Footer & Bump Navbar

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 export const NavBar: FC = () => {
     return (
-        <div className="w-full p-8 pb-4 pt-6 md:pt-6 md:pb-4 md:px-8 flex justify-between">
+        <head className="w-full p-8 pb-4 pt-6 md:pt-6 md:pb-4 md:px-8 flex justify-between">
             <div className="flex flex-col justify-center">
                 <div className="bg-notblack w-fit font-bold">
                     <span className="text-white -1 mx-3">v3xlabs</span>
@@ -30,6 +30,6 @@ export const NavBar: FC = () => {
                     ))}
                 </ul>
             </nav>
-        </div>
+        </head>
     );
 };
