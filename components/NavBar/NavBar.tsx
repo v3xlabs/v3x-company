e4f87e1c942273db66412f523c435c07503039a2
Jonatan <jonatan@jontes.page>
2024-05-12 19:25:58 +0200

    Update Navbar Links (#8)
    Co-authored-by: Luc <luc@lucemans.nl>

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 export const NavBar: FC = () => {
     return (
-        <header className="lg:absolute lg:left-0 w-auto p-8 flex flex-col gap-4 max-w-xs">
+        <header className="2xl:absolute 2xl:left-0 w-auto p-8 flex flex-col gap-4 max-w-xs">
             <div className="flex flex-col justify-center whitespace-nowrap">
                 <div className="bg-notblack w-fit font-bold">
                     <span className="text-white -1 mx-3">v3xlabs</span>
@@ -13,13 +13,11 @@ export const NavBar: FC = () => {
                 <ul className="">
                     {[
                         ['.company', 'https://v3x.company'],
-                        ['.contact', 'https://v3x.contact'],
                         ['.domains', 'https://v3x.domains'],
                         ['.health', 'https://v3x.health'],
                         ['.store', 'https://v3x.store'],
                         ['.team', 'https://v3x.team'],
-                        ['.chat', 'https://v3x.chat'],
-                        ['.wiki', 'https://v3x.wiki'],
+                        // ['.chat', 'https://v3x.chat'],
                         ['.vc', 'https://v3x.vc'],
                     ].map(([label, href]) => (
                         <li key={label}>
